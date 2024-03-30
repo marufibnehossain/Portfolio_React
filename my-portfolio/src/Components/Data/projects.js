c664768b4447649f2327c33885dd49482a9fa330
Maruf <marufibnhossain@gmail.com>
2024-03-30 11:57:39 +0600

    projects added

--- my-portfolio/src/Components/Data/projects.js
+++ my-portfolio/src/Components/Data/projects.js
@@ -8,11 +8,70 @@ import fajrBanner from '../../assets/images/FajrAdvertising/fajrBg.webp';
 import advancedLogo from '../../assets/images/Advanced/logo-advanced.webp';
 import hakkaniLogo from '../../assets/images/Hakkani/hakkanilogo.png';
 import hakkaniBg from '../../assets/images/Hakkani/hakkaniBG.webp';
+import igpBanner from '../../assets/images/Edu-IGP/igp-cover.png';
+import fajrlLogo from '../../assets/images/fajr-lifestyle/Fajr-lifestyle-logo.png';
+import fajrlBanner from '../../assets/images/fajr-lifestyle/fajrl-banner.jpg';
+import atomyBanner from '../../assets/images/Atomy-BD/atomy-banner.png';
+import atomyLogo from '../../assets/images/Atomy-BD/atomy-logo.png';
 
 
 export const projectsData = [
     {
         id: 1,
+        image: igpBanner,
+        logoimg: "",
+        altlogo: "EduIGP",
+        role: "Web developer",
+        project: "A Website Showcasing an online edication platform and it's members.",
+        intro: "I developed a captivating website for EduIGP. The website showcased their platform members.",
+        buttonw: "No case study",
+        buttonb: "Visit Website",
+        textclr: "black",
+        visiblewht: "inline-block",
+        visibleblk: "inline-block",
+        caselink: "",
+        sitelink: "https://z.altijaarat.com/",
+        category: "WordPress",
+        featured: "true"
+    },
+    {
+        id: 2,
+        image: fajrlBanner,
+        logoimg: fajrlLogo,
+        altlogo:"Fajr Lifestyle",
+        role: "Web Developer",
+        project: "An E-Commerce website for elegent shirts.",
+        intro: "The Fajr Lifestyle website is a Woocommerce website that sells some awesome formal shirts.",
+        buttonw: "No CASE STUDY",
+        buttonb: "Visit Website",
+        textclr: "",
+        visiblewht: "inline-block",
+        visibleblk: "inline-block",
+        caselink: "",
+        sitelink: "https://fajrlifestyle.com/",
+        category: "WordPress",
+        featured: "true"
+    },
+    {
+        id: 3,
+        image: atomyBanner,
+        logoimg: atomyLogo,
+        altlogo:"Atomy-BD",
+        role: "Web Developer",
+        project: "An E-Commerce website for Atomy skin products.",
+        intro: "The Atomy-Bangladesh website is a Woocommerce website that sells Atomy skin products.",
+        buttonw: "No CASE STUDY",
+        buttonb: "Visit Website",
+        textclr: "",
+        visiblewht: "inline-block",
+        visibleblk: "inline-block",
+        caselink: "",
+        sitelink: "https://atomy-bd.com/",
+        category: "WordPress",
+        featured: "true"
+    },
+    {
+        id: 4,
         image: fajrBanner,
         logoimg: fajrLogo,
         altlogo:"Fajr Advertising",
@@ -25,12 +84,12 @@ export const projectsData = [
         visiblewht: "inline-block",
         visibleblk: "inline-block",
         caselink: "fajr-advertising",
-        sitelink: "https://fajr.altijaarat.com/",
+        sitelink: "https://fajradvertising.com/",
         category: "WordPress",
         featured: "false"
     },
     {
-        id: 2,
+        id: 5,
         image: containers,
         logoimg: advancedLogo,
         altlogo: "advanced engineering",
@@ -48,7 +107,7 @@ export const projectsData = [
         featured: "true"
     },
     {
-        id: 3,
+        id: 6,
         image: hakkaniBg,
         logoimg: hakkaniLogo,
         altlogo: "Hakkani Group",
@@ -66,7 +125,7 @@ export const projectsData = [
         featured: "false"
     },
     {
-        id: 4,
+        id: 7,
         image: smileBanner,
         logoimg: smileLogo,
         altlogo: "Smile tissue",
@@ -84,7 +143,7 @@ export const projectsData = [
         featured: "true"
     },
     {
-        id: 5,
+        id: 8,
         image: saBanner,
         logoimg: salmanLogo,
         altlogo: "Salman Arefin",
@@ -100,7 +159,8 @@ export const projectsData = [
         sitelink: "https://salmanarefin.ca/",
         category: "WordPress",
         featured: "true"
-    }
+    },
+    
 ]
 
 
