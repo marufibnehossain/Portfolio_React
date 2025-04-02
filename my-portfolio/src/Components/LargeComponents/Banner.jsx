c525ca0d307fd2417320c0795017d2bb1f6b49e1
Maruf <marufibnhossain@gmail.com>
2025-04-02 14:39:34 +0600

    projects added

--- my-portfolio/src/Components/LargeComponents/Banner.jsx
+++ my-portfolio/src/Components/LargeComponents/Banner.jsx
@@ -23,7 +23,7 @@ const Banner = () => {
       <div className='lg:w-9/12 md:w-10/12 text-white text-left z-20 absolute mt-24 xs:mx-0 mx-3'>
         <h1 className='sm:text-[40px] 2xs:text-[24px] text-[20px] tracking-wider font-archivo font-extrabold'>Maruf Ibne Hossain is a Web Developer who loves to craft a good Website.</h1>
         <img className='pt-8 w-16 h-full' src={horizontalLine} alt="" />
-        <p className='sm:text-[#868686] text-white md:font-normal font-semibold font-nunito pt-8 text-lg xs:w-3/5'>I do Web Development and Machine Learning Research. Currently, I'm a Web Developer at <br /><Link to={'#'} target="_blank" className='text-[#C89B1E] font-bold'>Sleek Solutions LTD</Link>.</p>
+        <p className='sm:text-[#868686] text-white md:font-normal font-semibold font-nunito pt-8 text-lg xs:w-3/5'>I do Web Development and Machine Learning Research. Currently, I'm a Web Developer at <br /><Link to={'https://techbucketbd.com/'} target="_blank" className='text-[#C89B1E] font-bold'>Tech Bucket</Link>.</p>
         <div className='md:text-[#B5B5B5] text-[#cca354] relative text-[28px] flex gap-5 mt-6'>
           <Link to={'https://github.com/marufibnehossain'} target={'_blank'} aria-label="Visit my GitHub profile"><FaGithub className='md:hover:text-[#cca354] transition-all duration-300' /></Link>
           <Link to={'https://www.linkedin.com/in/marufibnhossain/'} target={'_blank'} aria-label="Visit my Linkedin profile"><FaLinkedin className='md:hover:text-[#cca354] transition-all duration-300' /></Link>
